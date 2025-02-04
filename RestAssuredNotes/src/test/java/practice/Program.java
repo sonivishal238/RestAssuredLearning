1108d966ca3f7e1a20cb4e08d1ee5d2790f1f72b
Vishal Soni <sonivishal238@gmail.com>
2025-02-04 15:50:41 +0530

    added practice package

---
+++ RestAssuredNotes/src/test/java/practice/Program.java
@@ -0,0 +1,22 @@
+package practice;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+	
+
+public class Program{
+	@Test
+	public void Test() {
+		
+
+		 
+		
+	}
+}
